4cb7099e67335fb377e5ce0d950d26adb240c6c0
jlaughlin2007-2 <86381068+jlaughlin2007-2@users.noreply.github.com>
2021-07-16 10:29:53 -1000

    Add files via upload

---
+++ Project 27/MatterJSBoilerPlate-master/rope.js
@@ -0,0 +1,21 @@
+class rope{
+    constructor(body1,body2, pointA, pointB){
+        this.pointA=pointA
+		this.pointB=pointB
+        this.options = {
+            bodyA:body1,
+            bodyB:body2,
+            length:250,
+            pointB:{x:this.pointA, y:this.pointB}
+        }
+        this.rope = Constraint.create(this.options);
+        World.add(world,this.rope); 
+    }
+    display(){
+       
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
+        strokeWeight(2);
+		line(pointA.x,pointA.y,pointB.x+this.pointA,pointB.y+this.pointB);
+    }
+}
